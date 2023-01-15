43b1e561ebeb2d68dc2779e0e1ca58ba355a681a
cabaelamirxle <amirxlecabael28@gmail.com>
2023-01-15 12:04:23 +0800

    self.left done

--- MyNameInBinaryTree.py
+++ MyNameInBinaryTree.py
@@ -3,3 +3,13 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
+
+    def insert(self, data):
+        if self.data == None:
+            self.data = data
+        else:
+            if self.left < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
