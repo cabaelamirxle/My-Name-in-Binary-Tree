0941843d4c1e8769a906c1423761870896dbd5ab
cabaelamirxle <amirxlecabael28@gmail.com>
2023-01-15 12:10:27 +0800

    root node and root insert done

--- MyNameInBinaryTree.py
+++ MyNameInBinaryTree.py
@@ -18,3 +18,18 @@ class Node:
                     self.right = Node(data)
                 else:
                     self.right.insert(data)
+
+if __name__ == '__main__':
+    root = Node('m')
+    root.insert ('a')
+    root.insert ('i')
+    root.insert ('r')
+    root.insert ('x')
+    root.insert ('l')
+    root.insert ('e')
+    root.insert ('c')
+    root.insert ('a')
+    root.insert ('b')
+    root.insert ('a')
+    root.insert ('e')
+    root.insert ('l')
